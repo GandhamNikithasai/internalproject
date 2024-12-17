79bcd55b0edfbe2ad9ce7a5d91c02dc0e6ff811e
GandhamNikithasai <nikithasaipatel@gmail.com>
2024-12-17 15:02:37 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,10 +1,11 @@
 public class addition{
-	public static void main(String args[])
+	public static void main(String[] args)
 		//hardcoded values for the two numbers
 		int num1=10;
 	int num2=20;
-	//performin addition
+	//performing addition
 	int sum=num1+num2;
 		//displaying the result
-	System.out.println("the sum of the numbers "+num1+"&"+num2+" is:",+sum);}
+	System.out.println("The sum of the numbers "+num1+"&"+num2+" is:",+sum);
+}
 	}
