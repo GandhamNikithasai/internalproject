6829bc07a04f5fdb236e88712dc0785cc48110ae
nikitha <nikitha@gmail.com>
2024-12-17 09:00:54 +0530

    addition of two numbers

---
+++ addition.java
@@ -0,0 +1,10 @@
+public class addition{
+	public static void main(String args[])
+		//hardcoded values for the two numbers
+		int num1=10;
+	int num2=20;
+	//performin addition
+	int sum=num1+num2;
+		//displaying the result
+	System.out.println("the sum of the numbers "+num1+"&"+num2+" is:",+sum);}
+	}
